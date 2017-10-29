6d636ce27fc0e3ad20e95d5d95925531fb788a1f
Paul Bourven <paul.bourven@epitech.eu>
2017-10-29 19:46:02 -0400

    Add Body script with simple declaration of colors

---
+++ Assets/Scripts/Body.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour {
+
+    [SerializeField]
+    private float moveForce;
+    [SerializeField]
+    private float jumpForce;
+
+    void Red()
+    {
+        moveForce *= 2;
+        jumpForce *= 2;
+    }
+
+    void Orange()
+    {
+
+    }
+
+    void Yellow()
+    {
+
+    }
+
+    void Green()
+    {
+
+    }
+
+    void Blue()
+    {
+
+    }
+
+    void Purple()
+    {
+
+    }
+}
