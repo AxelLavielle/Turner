aa2f2eed088499ab516f2d0f333881d74ac09f6b
Paul Bourven <paul.bourven@epitech.eu>
2017-10-29 20:18:47 -0400

    Completed color functions in Body (not final version), except Yellow Added variables moveForceNeutral and jumpForceNeutral to Body, to set default value of the body physics.

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class Body : MonoBehaviour {
 
     [SerializeField]
+    private float moveForceNeutral;
+    [SerializeField]
+    private float jumpForceNeutral;
+
+    [SerializeField]
     protected float moveForce;
     [SerializeField]
     protected float jumpForce;
 
     public void Neutral()
     {
-
+        moveForce = moveForceNeutral;
+        jumpForce = jumpForceNeutral;
     }
 
     public void Red()
     {
-        moveForce *= 2;
-        jumpForce *= 2;
+        moveForce = moveForceNeutral * 2;
+        jumpForce = jumpForceNeutral * 2;
     }
 
     public void Orange()
     {
-
+        moveForce = moveForceNeutral / 2;
     }
 
     public void Yellow()
@@ -32,17 +38,17 @@ public class Body : MonoBehaviour {
 
     public void Green()
     {
-
+        moveForce = 0;
     }
 
     public void Blue()
     {
-
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForceNeutral, ForceMode2D.Force);
     }
 
     public void Purple()
     {
-
+        moveForce = moveForceNeutral / 2;
     }
 
 }
