f30e2ef48119f6e609b1c900a0a4895ae32a4e17
Axel 'Loess' Lavielle <lavielleaxel@gmail.com>
2017-10-31 14:49:55 -0400

    rename background and add plateform script to trap and  remove useless script for enemy and reimport enemy sprite corectly / collisionbox

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    public Transform m_Target;
-
-    public float m_FollowSpeed;
-
-    public float m_FollowRange;
-
-    public float m_ArriveThreshold;
 	// Use this for initialization
 	void Start () {
-        m_Target = GameObject.FindWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (m_Target != null)
-        {
-            float distance = Vector3.Distance(m_Target.position, transform.position);
-            if (distance < m_FollowRange && distance > m_ArriveThreshold)
-            {
-                transform.Translate((m_Target.position - transform.position) * Time.deltaTime * m_FollowSpeed, Space.World);
-            }
-        }
 	}
 }
