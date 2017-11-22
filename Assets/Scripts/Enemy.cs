8bf7c894637d4290e795c5dd9d3ab58a5db7f356
Axel 'Loess' Lavielle <lavielleaxel@gmail.com>
2017-11-22 15:01:33 -0500

    Add the enemies behavior

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    [SerializeField]
+    Vector2 offset;
+    [SerializeField]
+    float speed;
+    int direction = -1;
+    BoxCollider2D box;
+    Rigidbody2D Rigidbody;
+    Vector2 oldPosition;
+    bool idle = true;
 
 	// Use this for initialization
 	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+        box = GetComponent<BoxCollider2D>();
+        Rigidbody = GetComponent<Rigidbody2D>();
+        oldPosition = transform.position;
+    }
+
+    private void Flip()
+    {
+        //We change the side of looking
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (!idle)
+        {
+            print(Rigidbody.velocity);
+            if (Rigidbody.velocity.y < -0.0001)
+            {
+                Flip();
+                direction *= -1;
+                transform.position = oldPosition;
+            }
+            oldPosition = transform.position;
+            transform.position = new Vector2(transform.position.x + speed * direction, transform.position.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            idle = false;
+        }
+    }
+
 }
