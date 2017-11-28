ad9fffb92e0ebb2073a3b150ab0f9da56dab8248
alicepesty <alice.pesty@epitech.eu>
2017-11-28 17:53:41 -0500

    automatic next level + flashlight colors

---
+++ Assets/Scripts/Level1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level1 : MonoBehaviour
+{
+    Player player;
+    GameObject proj;
+    GameObject redLight;
+    public Text tipsText;
+    bool getRed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Player>();
+        getRed = false;
+        tipsText.text = "Pick up the red color!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTipsText();
+    }
+
+    private void SetTipsText()
+    {
+        if (player.colorNb() > 0)
+        {
+            tipsText.text = "Point the light at the ground to speed up and jump higher. \r\nGo to the destination";
+        }
+
+        if (transform.position.x > 2600f)
+        {
+            tipsText.text = "Bravo! Go to next level!";
+        }
+
+    }
+
+
+}
