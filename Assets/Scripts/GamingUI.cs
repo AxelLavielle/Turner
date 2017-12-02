1e24ad8d31cc89fd580704a654c50ae761f360c2
cZhen42 <chaobinzhen@gmail.com>
2017-12-02 12:15:22 -0500

    Delete score text in Gaming UI and make it a prefab

--- Assets/Scripts/GamingUI.cs
+++ Assets/Scripts/GamingUI.cs
@@ -5,33 +5,24 @@ using UnityEngine.UI;
 
 public class GamingUI : MonoBehaviour {
     private float timer;
-    private int score;
     public Text timerText;
-    public Text scoreText;
     public GameObject pauseMenu;
 
 	// Use this for initialization
     void Start () {
         timer = 0f;
-        score = 0;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         TimerText();
-        ScoreText();
 	}
 
     private void TimerText()
     {
         timer += Time.deltaTime;
         timerText.text = "Time: " + Mathf.Floor(timer).ToString();
-    }
-
-    private void ScoreText()
-    {
-        scoreText.text = "Score: " + score;
     }
 
     public void Pause()
