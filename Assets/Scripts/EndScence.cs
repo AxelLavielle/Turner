f7f1e611975c9cecd58698965df3be3e350b59f1
alicepesty <alice.pesty@epitech.eu>
2017-12-02 14:42:59 -0500

    End scene

---
+++ Assets/Scripts/EndScence.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndScence : MonoBehaviour {
+
+
+    public void level1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
