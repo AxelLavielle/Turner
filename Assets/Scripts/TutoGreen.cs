57328b93b6eb494d48899caf13909295bba2e89e
Paul Bourven <paul.bourven@epitech.eu>
2017-12-02 12:59:26 -0500

    Add tips text to tuto green

---
+++ Assets/Scripts/TutoGreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutoGreen : MonoBehaviour
+{
+    Player player;
+    GameObject proj;
+    GameObject blueLight;
+    public Text tipsText;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTipsText();
+    }
+
+
+    private void SetTipsText()
+    {
+        if (player.ColorNb() > 1)
+        {
+            tipsText.text = "You can mix blue and yellow to obtain the color green by pressing key 2, 3 then e.\r\nGreen makes everythings stick to the surface where it is, use it to walljump and more!";
+        }
+
+        if (transform.position.x > 3400f)
+        {
+            tipsText.text = "Bravo! Go to next level!\r\n";
+        }
+
+    }
+}
