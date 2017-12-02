6d7b7e749dcd65e1c78114099470ec4da4c1caf1
alicepesty <alice.pesty@epitech.eu>
2017-12-02 12:51:49 -0500

    modified canvas prefab + tips to level 7

---
+++ Assets/Scripts/Level7.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Level7 : MonoBehaviour {
+    Player player;
+    GameObject proj;
+    GameObject blueLight;
+    public Text tipsText;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTipsText();
+    }
+
+
+    private void SetTipsText()
+    {
+        if (player.ColorNb() > 2)
+        {
+            tipsText.text = "You can mix blue and yellow to obtain the color green by pressing key 2, 3 then e.\r\nGreen allows you to stick to walls and jump on them!";
+        }
+
+        if (transform.position.x > 1675f)
+        {
+            tipsText.text = "Bravo! Go to next level!\r\n";
+        }
+
+    }
+
+}
